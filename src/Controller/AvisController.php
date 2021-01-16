34baf3bde8f09be5bb55e7313310b9383abaad1c
Mohamed Jedny <mohamed.jedny.etu@univ-lille.fr>
2021-01-16 15:44:57 +0100

    Add show page and navigation between pages

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -6,16 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AvisRepository;
+use App\Entity\Avis;
 
 class AvisController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(AvisRepository $avisRepository)
+    public function index(AvisRepository $avisRepository): Response
     {
         $avis = $avisRepository->findAll();
         
         return $this->render('avis/index.html.twig', compact('avis'));
+    }
+
+    /**
+     * @Route("/avis/{id<[0-9]+>}", name="app_avis_show")
+     */
+    public function show(Avis $avis): Response
+    {
+        return $this->render('avis/show.html.twig', compact('avis'));
     }
 }
