e4e7c89428c00fa29c3d44736f39570f2e54d44e
Mohamed Jedny <mohamed.jedny.etu@univ-lille.fr>
2021-01-16 18:43:52 +0100

    add create/edit form and basic navigation

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AvisRepository;
 use App\Entity\Avis;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AvisController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home" , methods="GET")
      */
     public function index(AvisRepository $avisRepository): Response
     {
@@ -21,10 +25,61 @@ class AvisController extends AbstractController
     }
 
     /**
-     * @Route("/avis/{id<[0-9]+>}", name="app_avis_show")
+     * @Route("/avis/create", name="app_avis_create" , methods={"GET","POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $avis = new Avis;
+
+        $form = $this->createFormBuilder($avis)
+                ->add('titre', TextType::class)
+                ->add('description', TextareaType::class)
+                ->getForm()
+        ;
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->persist($avis);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('avis/create.html.twig', [
+            'monForm' => $form->createView()
+            ]);
+    }
+
+    /**
+     * @Route("/avis/{id<[0-9]+>}", name="app_avis_show" , methods="GET")
      */
     public function show(Avis $avis): Response
     {
         return $this->render('avis/show.html.twig', compact('avis'));
+    }
+
+    /**
+     * @Route("/avis/{id<[0-9]+>}/edit", name="app_avis_edit" , methods="GET|POST")
+     */
+    public function edit(Request $request, Avis $avis, EntityManagerInterface $em): Response
+    {
+        $form = $this->createFormBuilder($avis)
+                ->add('titre', TextType::class)
+                ->add('description', TextareaType::class)
+                ->getForm()
+        ;
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('avis/edit.html.twig', [
+            'avis' => $avis,
+            'monForm' => $form->createView()
+            ]);
     }
 }
